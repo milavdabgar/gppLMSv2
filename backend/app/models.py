ef32c563ec7b7ac58017bcf414e829b28d01964d
Milav Dabgar <milav.dabgar@gmail.com>
2024-01-18 20:58:27 +0530

    fixing user apis

--- backend/app/models.py
+++ backend/app/models.py
@@ -2,11 +2,20 @@ from app.extensions import db
 from datetime import datetime, timedelta
 from flask_security import UserMixin, RoleMixin, SQLAlchemyUserDatastore
 
-class CRUDMixin:
+
+class CRUDMixin(db.Model):
+    """
+    Mixin that adds convenience methods for CRUD
+    (create, read, update, delete) operations.
+    """
+
+    __abstract__ = True  # declares this as an abstract class
+
     @classmethod
     def create(cls, **kwargs):
         instance = cls(**kwargs)
         db.session.add(instance)
+        db.session.commit()
         return instance
 
     @classmethod
@@ -17,81 +26,80 @@ class CRUDMixin:
     def get_all(cls):
         return cls.query.all()
 
-    def update(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    @classmethod
+    def find_by(cls, **kwargs):
+        return cls.query.filter_by(**kwargs).all()
 
-    def delete(self):
+    def update(self, commit=True, **kwargs):
+        for attr, value in kwargs.items():
+            setattr(self, attr, value)
+        if commit:
+            db.session.commit()
+        return self
+
+    def delete(self, commit=True):
         db.session.delete(self)
+        if commit:
+            db.session.commit()
 
+    # Optional: Implement a method to serialize data, useful for APIs
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
-# class CRUDMixin(object):
-#     """Mixin for CRUD operations."""
-
-#     @classmethod
-#     def create(cls, **kwargs):
-#         instance = cls(**kwargs)
-#         return instance.save()
-
-#     def update(self, commit=True, **kwargs):
-#         for attr, value in kwargs.items():
-#             setattr(self, attr, value)
-#         return commit and self.save() or self
-
-#     def save(self, commit=True):
-#         db.session.add(self)
-#         if commit:
-#             db.session.commit()
-#         return self
-
-#     def delete(self, commit=True):
-#         db.session.delete(self)
-#         return commit and db.session.commit()
 
 # Association tables for many-to-many relationships
-roles_users = db.Table('roles_users',
-    db.Column('role_id', db.Integer(), db.ForeignKey('role.id')),
-    db.Column('user_id', db.Integer(), db.ForeignKey('user.id'))
+roles_users = db.Table(
+    "roles_users",
+    db.Column("role_id", db.Integer(), db.ForeignKey("role.id")),
+    db.Column("user_id", db.Integer(), db.ForeignKey("user.id")),
 )
 
-member_books = db.Table('member_books',
-    db.Column('member_id', db.Integer(), db.ForeignKey('member.id')),
-    db.Column('book_id', db.Integer(), db.ForeignKey('book.id'))
+member_books = db.Table(
+    "member_books",
+    db.Column("member_id", db.Integer(), db.ForeignKey("member.id")),
+    db.Column("book_id", db.Integer(), db.ForeignKey("book.id")),
 )
 
 
-membership_books = db.Table('membership_books',
-    db.Column('membership_id', db.Integer(), db.ForeignKey('membership.id')),
-    db.Column('book_id', db.Integer(), db.ForeignKey('book.id'))
+membership_books = db.Table(
+    "membership_books",
+    db.Column("membership_id", db.Integer(), db.ForeignKey("membership.id")),
+    db.Column("book_id", db.Integer(), db.ForeignKey("book.id")),
 )
 
-wishlist_books = db.Table('wishlist_books',
-    db.Column('wishlist_id', db.Integer(), db.ForeignKey('wishlist.id')),
-    db.Column('book_id', db.Integer(), db.ForeignKey('book.id'))
+wishlist_books = db.Table(
+    "wishlist_books",
+    db.Column("wishlist_id", db.Integer(), db.ForeignKey("wishlist.id")),
+    db.Column("book_id", db.Integer(), db.ForeignKey("book.id")),
 )
 
-collection_books = db.Table('collection_books',
-    db.Column('collection_id', db.Integer(), db.ForeignKey('collection.id')),
-    db.Column('book_id', db.Integer(), db.ForeignKey('book.id'))
+collection_books = db.Table(
+    "collection_books",
+    db.Column("collection_id", db.Integer(), db.ForeignKey("collection.id")),
+    db.Column("book_id", db.Integer(), db.ForeignKey("book.id")),
 )
 
-authors_books = db.Table('authors_books',
-    db.Column('author_id', db.Integer(), db.ForeignKey('author.id')),
-    db.Column('book_id', db.Integer(), db.ForeignKey('book.id'))
+authors_books = db.Table(
+    "authors_books",
+    db.Column("author_id", db.Integer(), db.ForeignKey("author.id")),
+    db.Column("book_id", db.Integer(), db.ForeignKey("book.id")),
 )
 
-genres_books = db.Table('genres_books',
-    db.Column('genre_id', db.Integer(), db.ForeignKey('genre.id')),
-    db.Column('book_id', db.Integer(), db.ForeignKey('book.id'))
+genres_books = db.Table(
+    "genres_books",
+    db.Column("genre_id", db.Integer(), db.ForeignKey("genre.id")),
+    db.Column("book_id", db.Integer(), db.ForeignKey("book.id")),
 )
 
-class Role(db.Model, RoleMixin, CRUDMixin):
+
+class Role(RoleMixin, CRUDMixin):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
 
-class User(db.Model, UserMixin, CRUDMixin):
-    __mapper_args__ = {'polymorphic_identity': 'user', 'polymorphic_on': 'type'}
+
+class User(UserMixin, CRUDMixin):
+    __mapper_args__ = {"polymorphic_identity": "user", "polymorphic_on": "type"}
     id = db.Column(db.Integer, primary_key=True)
     fs_uniquifier = db.Column(db.String(255), unique=True, nullable=False)
     username = db.Column(db.String(80), unique=True)
@@ -106,58 +114,69 @@ class User(db.Model, UserMixin, CRUDMixin):
     )
     type = db.Column(db.String(50))
 
+
 class Admin(User):
-    __mapper_args__ = {'polymorphic_identity': 'admin'}
-    id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    __mapper_args__ = {"polymorphic_identity": "admin"}
+    id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
+
 
 class Librarian(User):
-    __mapper_args__ = {'polymorphic_identity': 'librarian'}
-    id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    __mapper_args__ = {"polymorphic_identity": "librarian"}
+    id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
+
 
 class Member(User):
-    __mapper_args__ = {'polymorphic_identity': 'member'}
-    id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    membership_id = db.Column(db.Integer, db.ForeignKey('membership.id'))
+    __mapper_args__ = {"polymorphic_identity": "member"}
+    id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
+    membership_id = db.Column(db.Integer, db.ForeignKey("membership.id"))
     preferred_genres = db.Column(db.String(255))  # Store comma-separated genre IDs
-    language_preference = db.Column(db.String(50))    
+    language_preference = db.Column(db.String(50))
     # Relationships
-    reading_history = db.relationship('Book', secondary=member_books, backref='member', lazy='dynamic')
-    transactions = db.relationship('Transaction', backref='member', lazy='dynamic')
-    book_loans = db.relationship('BookLoan', backref='member', lazy='dynamic')
-    purchases = db.relationship('Purchase', backref='member', lazy='dynamic')
-    wishlists = db.relationship('Wishlist', backref='member', lazy='dynamic')
-    collections = db.relationship('Collection', backref='member', lazy='dynamic')
-    reviews = db.relationship('Review', backref='member', lazy='dynamic')
+    reading_history = db.relationship(
+        "Book", secondary=member_books, backref="member", lazy="dynamic"
+    )
+    transactions = db.relationship("Transaction", backref="member", lazy="dynamic")
+    book_loans = db.relationship("BookLoan", backref="member", lazy="dynamic")
+    purchases = db.relationship("Purchase", backref="member", lazy="dynamic")
+    wishlists = db.relationship("Wishlist", backref="member", lazy="dynamic")
+    collections = db.relationship("Collection", backref="member", lazy="dynamic")
+    reviews = db.relationship("Review", backref="member", lazy="dynamic")
 
-class Membership(db.Model, CRUDMixin):
+
+class Membership(CRUDMixin):
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.String(50))  # Silver, Gold, Platinum
     price = db.Column(db.Float)
     duration_type = db.Column(db.String(50))  # monthly, yearly, lifetime
     max_books_allowed = db.Column(db.Integer)
     discount_rate = db.Column(db.Float)  # Discount on purchases
-    members = db.relationship('Member', backref='membership', lazy='dynamic')
+    members = db.relationship("Member", backref="membership", lazy="dynamic")
 
-class Wishlist(db.Model, CRUDMixin):
+
+class Wishlist(CRUDMixin):
     id = db.Column(db.Integer, primary_key=True)
-    member_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    member_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
-class Collection(db.Model, CRUDMixin):
+
+class Collection(CRUDMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    member_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    member_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
-class Genre(db.Model, CRUDMixin):
+
+class Genre(CRUDMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True)
     description = db.Column(db.Text)
 
-class Author(db.Model, CRUDMixin):
+
+class Author(CRUDMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     biography = db.Column(db.String, nullable=False)
 
-class Book(db.Model, CRUDMixin):
+
+class Book(CRUDMixin):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text)  # Use appropriate storage for book content
@@ -167,18 +186,29 @@ class Book(db.Model, CRUDMixin):
     publication_date = db.Column(db.Date)
     language = db.Column(db.String(50))
     type = db.Column(db.String(50))  # Physical, eBook, Audiobook
-    
+
     # Relationships: Many to Many
-    authors = db.relationship("Author", secondary=authors_books, backref="books", lazy='dynamic')
-    genres = db.relationship('Genre', secondary=genres_books, backref='books', lazy='dynamic')
-    collections = db.relationship('Collection', secondary=collection_books, backref='books', lazy='dynamic')
-    wishlists = db.relationship("Wishlist", secondary=wishlist_books, backref="book", lazy="dynamic")
-    free_access_in_memberships = db.relationship("Membership", secondary=membership_books, backref="book", lazy="dynamic")
+    authors = db.relationship(
+        "Author", secondary=authors_books, backref="books", lazy="dynamic"
+    )
+    genres = db.relationship(
+        "Genre", secondary=genres_books, backref="books", lazy="dynamic"
+    )
+    collections = db.relationship(
+        "Collection", secondary=collection_books, backref="books", lazy="dynamic"
+    )
+    wishlists = db.relationship(
+        "Wishlist", secondary=wishlist_books, backref="book", lazy="dynamic"
+    )
+    free_access_in_memberships = db.relationship(
+        "Membership", secondary=membership_books, backref="book", lazy="dynamic"
+    )
     # Relationships: One to Many
     transactions = db.relationship("Transaction", backref="book", lazy="dynamic")
     book_loans = db.relationship("BookLoan", backref="book", lazy="dynamic")
     purchases = db.relationship("Purchase", backref="book", lazy="dynamic")
     reviews = db.relationship("Review", backref="book", lazy="dynamic")
+
 
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -188,16 +218,22 @@ class Transaction(db.Model):
     date = db.Column(db.DateTime, default=datetime.utcnow)
     status = db.Column(db.String(20))  # 'active', 'completed'
 
-class BookLoan(db.Model, CRUDMixin):
+
+class BookLoan(CRUDMixin):
     id = db.Column(db.Integer, primary_key=True)
     book_id = db.Column(db.Integer, db.ForeignKey("book.id"), nullable=False)
     member_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     loan_date = db.Column(db.DateTime, default=datetime.utcnow)
-    due_date = db.Column(db.DateTime, default=lambda: datetime.utcnow() + timedelta(days=14))
+    due_date = db.Column(
+        db.DateTime, default=lambda: datetime.utcnow() + timedelta(days=14)
+    )
     returned_date = db.Column(db.DateTime)
-    status = db.Column(db.String(20), default="issued")  # Statuses like issued, returned, overdue
+    status = db.Column(
+        db.String(20), default="issued"
+    )  # Statuses like issued, returned, overdue
     fine = db.Column(db.Float, default=0.0)
     renewal_count = db.Column(db.Integer, default=0)
+
 
 class Purchase(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -207,7 +243,8 @@ class Purchase(db.Model):
     price = db.Column(db.Float)
     price_after_discount = db.Column(db.Float)
 
-class Review(db.Model, CRUDMixin):
+
+class Review(CRUDMixin):
     id = db.Column(db.Integer, primary_key=True)
     rating = db.Column(db.Integer)  # e.g., 1-5
     review = db.Column(db.Text)
--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,6 +1,7 @@
 from app.extensions import ma
 from marshmallow import fields
 from marshmallow_sqlalchemy.fields import Nested
+from .extensions import db
 
 from .models import (
     Role,
@@ -24,6 +25,7 @@ class RoleSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Role
         load_instance = True
+        sqla_session = db.session
 
 
 # class UserSchema(ma.SQLAlchemyAutoSchema):
@@ -33,85 +35,103 @@ class RoleSchema(ma.SQLAlchemyAutoSchema):
 #         exclude = ("fs_uniquifier",)
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
+    roles = ma.Nested(RoleSchema, many=True, only=['id', 'name'])
     class Meta:
         model = User
         load_instance = False
-        exclude = ("fs_uniquifier",)
-
-    # Explicitly define the roles field
-    roles = fields.List(fields.Integer())
+        sqla_session = db.session
+        exclude = ("fs_uniquifier", "password",)
+        # include_fk = True
 
 
 class LibrarianSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Librarian
         load_instance = True
+        sqla_session = db.session
 
 
 class MemberSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Member
         load_instance = True
+        sqla_session = db.session
 
 
 class MembershipSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Membership
         load_instance = True
+        sqla_session = db.session
 
 
 class WishlistSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Wishlist
         load_instance = True
+        sqla_session = db.session
 
 
 class CollectionSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Collection
         load_instance = True
+        sqla_session = db.session
 
 
 class GenreSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Genre
         load_instance = True
+        sqla_session = db.session
 
 
 class AuthorSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Author
         load_instance = True
+        sqla_session = db.session
 
 
-class BookSchema(ma.SQLAlchemyAutoSchema):
+class BookSchema(ma.SQLAlchemyAutoSchema):  
+    authors = ma.Nested(AuthorSchema, many=True, only=['id', 'name'])
+    genres = ma.Nested(GenreSchema, many=True, only=['id', 'name'])
+    collections = ma.Nested(CollectionSchema, many=True) 
     class Meta:
         model = Book
         load_instance = True
+        sqla_session = db.session
+        # include_fk = True
+        
+
 
 
 class TransactionSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Transaction
         load_instance = True
+        sqla_session = db.session
 
 
 class BookLoanSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = BookLoan
         load_instance = True
+        sqla_session = db.session
 
 
 class PurchaseSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Purchase
         load_instance = True
+        sqla_session = db.session
 
 
 class ReviewSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Review
         load_instance = True
+        sqla_session = db.session
 
 
 role_schema = RoleSchema()
